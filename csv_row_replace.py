8da5835330352c2226a5bbec92ce9ae114294f9a
HankYu269 <81675050+HankYu269@users.noreply.github.com>
2021-11-24 22:18:12 +0800

    Add files via upload

---
+++ csv_row_replace.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def csv_row_replace(df, cep):
+    cep = int(cep)
+    for i in range(len(df.iloc[4])):
+        df.iloc[4,i] = cep*(i-1)
+    df.iloc[4, 0] = 0
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+    input_path, cep = sys.argv[1], sys.argv[2]
+    df = pd.read_csv(input_path)
+    input_path = os.path.splitext(input_path)[0]
+    csv_row_replace(df, cep)
+    df.to_csv(input_path+"_corr.csv", index=None)
